9e2b13f41f3db3d54aaeb9bd78bc9eda344e2890
Ants245 <33928118+Ants245@users.noreply.github.com>
2017-11-23 10:32:11 +0000

    Add files via upload

---
+++ Games production coursework/Car racer project/conf.lua
@@ -0,0 +1,8 @@
+function love.conf(t)
+  t.window.fullscreen = true
+  t.window.fullscreentype = "exclusive"
+  t.title = "Car game"
+  t.author = "Anthony Giblin"
+  t.identity = "Coursework"
+  
+end
