0beece506363cd8239afeeb756bb33336335d177
JackCross <33515846+JackCross@users.noreply.github.com>
2017-12-15 15:56:53 +0000

    Update 15/12/2017
    GUI update, changed sound toggle (F1-F6), added countdown sounds("s")

---
+++ Car Racer 2/audio.lua
@@ -0,0 +1,18 @@
+  Countdown = love.audio.newSource("Audio/In-Game Audio/Countdown.mp3", "static")
+  Close = love.audio.newSource("Audio/In-Game Audio/Close.mp3", "static")
+  horn = love.audio.newSource("Audio/Horns/siren.mp3", "static")
+  horn2 = love.audio.newSource("Audio/Horns/Ahorn.mp3", "static")
+  horn3 = love.audio.newSource("Audio/Horns/Dixie.mp3", "static")
+  horn4 = love.audio.newSource("Audio/Horns/sal.mp3", "static")
+
+  player1 = love.audio.newSource("Audio/Player victory/player1.mp3", "static")
+  player2 = love.audio.newSource("Audio/Player victory/player2.mp3", "static")
+  player3 = love.audio.newSource("Audio/Player victory/player3.mp3", "static")
+  player4 = love.audio.newSource("Audio/Player victory/player4.mp3", "static")
+
+  music = love.audio.newSource("Audio/In-Game Audio/Tokyo Drift.mp3")
+  music2 = love.audio.newSource("Audio/In-Game Audio/Born.mp3")
+  music3 = love.audio.newSource("Audio/In-Game Audio/Dean.mp3")
+  music4 = love.audio.newSource("Audio/In-Game Audio/High.mp3")
+  music5 = love.audio.newSource("Audio/In-Game Audio/SPD.mp3")
+  music6 = love.audio.newSource("Audio/In-Game Audio/500 Miles.mp3")
